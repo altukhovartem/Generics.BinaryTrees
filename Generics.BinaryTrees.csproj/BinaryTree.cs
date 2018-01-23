7c8bd80539f6ca1365ad63a16a3d279bbff18df5
aaltukhov <aaltukhov@acumatica.com>
2018-01-23 19:56:25 +0300

    second commit

--- Generics.BinaryTrees.csproj/BinaryTree.cs
+++ Generics.BinaryTrees.csproj/BinaryTree.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,59 @@ using System.Threading.Tasks;
 
 namespace Generics.BinaryTrees
 {
-    
+
+    public class Node
+    {
+        Node Parent { get; set; }
+        Node Left { get; set; }
+        Node Right { get; set; }
+        public int Value { get; set; }
+
+        public Node(int value)
+        {
+            this.Value = value;
+        }
+
+    }
+
+    public class BinaryTree : IEnumerable
+    {
+        public Node Head { get; set; }
+
+        public int Count { get; private set; }
+
+        public BinaryTree(int head)
+        {
+            Head = new Node(head);
+            Count = 1;
+        }
+
+        public BinaryTree()
+        {
+            Head = null;
+            Count = 0;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public void Add(Node NewNode)
+        {
+            if (Count == 0)
+            {
+                Head = NewNode;
+                Count++;
+            }
+
+            else
+            {
+                if (Head)
+            }
+                
+        }
+    }
 }
